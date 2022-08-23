f616b63bd00613b4a7091a72f5dea9c777707502
HyeJin <ette9844@gmail.com>
2022-08-23 16:24:26 +0900

    Add features to table-edits.js
    - Add delete button options
    - Add delete callback option
    - Add data validation check callback option
    - Add number type input option

--- src/table-edits.js
+++ src/table-edits.js
@@ -1,16 +1,20 @@
-
-;(function ($, window, document, undefined) {
+(function ($, window, document, undefined) {
     var pluginName = "editable",
         defaults = {
             keyboard: true,
             dblclick: true,
-            button: true,
-            buttonSelector: ".edit",
+            editButton: true,
+            editButtonSelector: ".edit",
+            removeButton: true,
+            removeButtonSelector: ".remove",
             maintainWidth: true,
             dropdowns: {},
+            numbers: [],
             edit: function() {},
             save: function() {},
-            cancel: function() {}
+            checkValid: function() {return true;},
+            cancel: function() {},
+            remove: function() {}
         };
 
     function editable(element, options) {
@@ -25,7 +29,7 @@
 
     editable.prototype = {
         init: function() {
-            this.editing = false;
+            this.editing = true;
 
             if (this.options.dblclick) {
                 $(this.element)
@@ -33,17 +37,20 @@
                     .bind('dblclick', this.toggle.bind(this));
             }
 
-            if (this.options.button) {
-                $(this.options.buttonSelector, this.element)
+            if (this.options.editButton) {
+                $(this.options.editButtonSelector, this.element)
                     .bind('click', this.toggle.bind(this));
+            }
+            
+            if (this.options.removeButton) {
+                $(this.options.removeButtonSelector, this.element)
+                    .bind('click', this.remove.bind(this));
             }
         },
 
         toggle: function(e) {
             e.preventDefault();
-
-            this.editing = !this.editing;
-
+            
             if (this.editing) {
                 this.edit();
             } else {
@@ -70,7 +77,7 @@
                 }
 
                 if (field in instance.options.dropdowns) {
-                    input = $('<select></select>');
+                    input = $('<select name="' + field + '"></select>');
 
                     for (var i = 0; i < instance.options.dropdowns[field].length; i++) {
                         $('<option></option>')
@@ -81,8 +88,13 @@
                     input.val(value)
                          .data('old-value', value)
                          .dblclick(instance._captureEvent);
+                } else if (instance.options.numbers.indexOf(field) != -1) {
+                    input = $('<input type="number" name="' + field + '"/>')
+	                    .val(value)
+	                    .data('old-value', value)
+	                    .dblclick(instance._captureEvent);
                 } else {
-                    input = $('<input type="text" />')
+                    input = $('<input type="text" name="' + field + '"/>')
                         .val(value)
                         .data('old-value', value)
                         .dblclick(instance._captureEvent);
@@ -96,12 +108,16 @@
             });
 
             this.options.edit.bind(this.element)(values);
+            this.editing = !this.editing;
         },
 
         save: function() {
             var instance = this,
                 values = {};
 
+            if (!this.checkValid()) {
+            	return;
+            }
             $('td[data-field]', this.element).each(function() {
                 var value = $(':input', this).val();
 
@@ -112,8 +128,20 @@
             });
 
             this.options.save.bind(this.element)(values);
+            this.editing = !this.editing;
         },
 
+        checkValid: function() {
+        	var values = {};
+        	
+        	$('td[data-field]', this.element).each(function() {
+	            var value = $(':input', this).val();
+	            values[$(this).data('field')] = value;
+	        });
+
+            return this.options.checkValid.bind(this.element)(values);
+        },
+        
         cancel: function() {
             var instance = this,
                 values = {};
@@ -128,6 +156,12 @@
             });
 
             this.options.cancel.bind(this.element)(values);
+            this.editing = !this.editing;
+        },
+        
+        remove: function() {
+	        this.options.remove.bind(this.element)();
+	        $(this).remove();
         },
 
         _captureEvent: function(e) {
@@ -136,10 +170,8 @@
 
         _captureKey: function(e) {
             if (e.which === 13) {
-                this.editing = false;
                 this.save();
             } else if (e.which === 27) {
-                this.editing = false;
                 this.cancel();
             }
         }
